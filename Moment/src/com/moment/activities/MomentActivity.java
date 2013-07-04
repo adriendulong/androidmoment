61aa80691d4058e73cf797fb4f065c5b472298fe
Swann Macé <swann.mace@gmail.com>
2013-07-04 12:06:51 +0200

    Facebook SDK 3

--- Moment/src/com/moment/activities/MomentActivity.java
+++ Moment/src/com/moment/activities/MomentActivity.java
@@ -22,6 +22,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 import com.google.android.gcm.GCMRegistrar;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -45,6 +50,30 @@ public class MomentActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moment);
         fontNumans = Typeface.createFromAsset(getAssets(), "fonts/Numans-Regular.otf");
+
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                if (session.isOpened()) {
+
+                    // make request to the /me API
+                    Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+                        // callback after Graph API response with user object
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            if (user != null) {
+                                TextView welcome = (TextView) findViewById(R.id.textView);
+                                welcome.setText("Hello " + user.getName() + "!");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
 
         MomentApi.initialize(getApplicationContext());
 
