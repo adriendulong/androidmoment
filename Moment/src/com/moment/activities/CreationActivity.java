b444b1a34bba5b82fcf566c41c98de240066df79
Swann Macé <swann.mace@gmail.com>
2013-07-25 10:15:23 +0200

    Fix reload timeline from creation

--- Moment/src/com/moment/activities/CreationActivity.java
+++ Moment/src/com/moment/activities/CreationActivity.java
@@ -156,7 +156,7 @@ public class CreationActivity extends SherlockActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                finish();
                 return true;
         }
         return super.onOptionsItemSelected(item);
